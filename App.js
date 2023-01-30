0c34b4b076530e332949512396b427f7d7ff09af
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-01-30 19:18:08 +0900

    feat: 프로젝트 생성 및 스플래시 화면 추가
    feat: 프로젝트 생성 및 스플래시 화면 추가

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 2000);
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{backgroundColor: 'yellow'}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
