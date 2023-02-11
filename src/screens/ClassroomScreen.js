62f0f52659756fa051e55fc5d964b5b736d4c2d5
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-02-12 05:20:55 +0900

    feat: 네비게이션 스크린 전환 애니메이션 변경
    feat: 네비게이션 스크린 전환 애니메이션 변경

--- src/screens/ClassroomScreen.js
+++ src/screens/ClassroomScreen.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text } from "react-native";
+
+import Header from "../components/Header";
+import Calendar from "../components/Calendar/Calendar";
+
 const View = SafeAreaView;
 
-export default () => {
+export default ClassroomScreen = ({navigation}) => {
   return (
     <View>
+      <Header 
+        title="일정 관리"
+        navigation={navigation}
+        back="SelectScreen"
+      />
+      <Calendar />
       <Text>클래스룸 스크린입니다</Text>
     </View>
   );
