d290d3cc93382538bbd53baa9198ba963a4a4454
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-02-14 20:58:05 +0900

    feat: 리뷰 체크박스 구현
    feat: 리뷰 체크박스 구현

--- src/components/Review/CheckButton.js
+++ src/components/Review/CheckButton.js
@@ -3,8 +3,11 @@ import styled from "styled-components/native";
 
 export default CheckButton = () => {
   return (
-    <>
-    </>
+    <ButtonSection>
+      <ButtonBox>
+
+      </ButtonBox>
+    </ButtonSection>
   );
 }
 
@@ -14,4 +17,7 @@ const ButtonSection = styled.TouchableOpacity`
 
 const ButtonBox = styled.View`
   border: 1px solid red;
+  borderRadius: 30px;
+  width: 20px;
+  height: 20px;
 `;
