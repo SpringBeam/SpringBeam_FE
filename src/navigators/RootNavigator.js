3ada8d20977a142cdc785c7700c4247855c3cd95
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-02-14 15:23:30 +0900

    chore: 네비게이터 구성 수정
    chore: 네비게이터 구성 수정

--- src/navigators/RootNavigator.js
+++ src/navigators/RootNavigator.js
@@ -8,11 +8,6 @@ export default RootNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
-        name="TabNavigator"
-        component={TabNavigator}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
         name="LoginScreen"
         component={LoginScreen}
         options={{ headerShown: false }}
@@ -20,6 +15,11 @@ export default RootNavigator = () => {
       <Stack.Screen
         name="SignUpNavigator"
         component={SignUpNavigator}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="TabNavigator"
+        component={TabNavigator}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
