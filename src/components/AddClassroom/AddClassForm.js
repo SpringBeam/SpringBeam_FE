a2792ad0f20b639b428d10d882ef75f89b3617fc
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-02-28 20:06:50 +0900

    feat: PickerSelect styles 추가
    feat: PickerSelect styles 추가

--- src/components/AddClassroom/AddClassForm.js
+++ src/components/AddClassroom/AddClassForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
 import Picker from "react-native-picker-select";
 import styled from "styled-components/native";
 
@@ -16,10 +17,10 @@ export default AddClassForm = () => {
 
   return (
     <FormContainer>
-      <SubjectPicker
-        placeholder={{ label: "수업하는 과목을 선택해주세요."}}
+      <Picker
         value={subject}
         onValueChange={(item) => setSubject(item)}
+        placeholder={{ label: "수업하는 과목을 선택해주세요."}}
         items={[
           { label: "국어", value: "국어", key: "1"},
           { label: "수학", value: "수학", key: "2"},
@@ -27,9 +28,15 @@ export default AddClassForm = () => {
           { label: "사회", value: "사회", key: "4"},
           { label: "물리", value: "물리", key: "5"},
           { label: "화학", value: "화학 ", key: "6"},
+          { label: "생명과학", value: "생명과학", key: "7"},
+          { label: "논술", value: "논술", key: "8"},
+          { label: "중국어", value: "중국어", key: "9"},
+          { label: "독일어", value: "독일어", key: "10"},
         ]}
+        useNativeAndroidPickerStyle={false}
+        style={pickerSelectStyles}
       >
-      </SubjectPicker>
+      </Picker>
       {startTime && (
         <Temp>
           {startTime}
@@ -53,10 +60,6 @@ const FormContainer = styled.View`
   marginHorizontal: 10px;
 `;
 
-const SubjectPicker = styled(Picker)`
-  
-`;
-
 const Temp = styled.Text`
   margin: 10px;
   fontSize: 16px;
@@ -69,3 +72,27 @@ const ButtonContainer = styled.View`
   flexDirection: row;
   justifyContent: space-between;
 `;
+
+  const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+      fontSize: 16,
+      paddingVertical: 12,
+      paddingHorizontal: 0,
+      // borderWidth: 1,
+      // borderColor: 'gray',
+      // borderRadius: 4,
+      color: 'black',
+      paddingRight: 30 // to ensure the text is never behind the icon
+    },
+    inputAndroid: {
+      width: 320,
+      fontSize: 16,
+      paddingVertical: 8,
+      paddingHorizontal: 0,
+      // borderWidth: 0.5,
+      // borderColor: 'purple',
+      // borderRadius: 8,
+      color: 'black',
+      paddingRight: 30 // to ensure the text is never behind the icon
+    }
+  });
