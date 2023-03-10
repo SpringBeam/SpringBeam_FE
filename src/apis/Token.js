b77127422b21c73d29ea9f768d296158d399f761
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-03-10 14:08:13 +0900

    feat: AccessToken, RefreshToken API 구현 중
    feat: AccessToken, RefreshToken API 구현 중

--- src/apis/Token.js
+++ src/apis/Token.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 import moment from "moment";
+import {
+  setAccessToken,
+  getAccessToken,
+  setRefreshToken,
+  getRefreshToken,
+  setAuthentication,
+} from "../auth/index";
 
 export const API = axios.create({});
 
 API.intercepters.request.use(async (res) => {
-  let accessToken = ("수정");
+  let accessToken = getAccessToken();
 })
