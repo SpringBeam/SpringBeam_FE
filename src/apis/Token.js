6e7a8c45d0edc43ed39ddcf50ffe9431dee05da3
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-03-11 19:31:04 +0900

    feat: API Interceptors 설정
    feat: API Interceptors 설정

--- src/apis/Token.js
+++ src/apis/Token.js
@@ -10,7 +10,30 @@ import {
 
 export const API = axios.create({});
 
-// API.intercepters.request.use(async (res) => {
-//   let accessToken = getAccessToken();
-//   const refreshToken = getRefreshToken();
-// })
+const BASE_API = "";
+
+API.interceptors.request.use(async (res) => {
+  let accessToken = getAccessToken();
+  const refreshToken = getRefreshToken();
+  const accessTokenExpirationTime = localStorage.getItem("accessTokenExpirationTime");
+
+  if (refreshToken && moment(accessTokenExpirationTime).diff(moment()) < 0) {
+    const { data } = await axios.post(
+      `${BASE_API}/auth/refresh`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          refreshToken: refreshToken,
+        },
+      },
+    );
+    accessToken = data.accessToken;
+    setAccessToken(data.accessToken);
+    setRefreshToken(data.refreshToken);
+    localStorage.setItem(
+      "accessTokenExpirationTime",
+      moment().add(8, "minutes").format("yyyy-MM-DD HH:mm:ss"),
+    );
+  }
+});
