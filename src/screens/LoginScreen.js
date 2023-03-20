e53aeb8765b53ac547b22893fc77fe371d447561
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-03-20 20:12:21 +0900

    feat: createURL 추가 및 redirect_uri 설정
    feat: createURL 추가 및 redirect_uri 설정

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
 
@@ -7,13 +8,18 @@ import OAuthLoginButton from "../components/Buttons/OAuthLoginButton";
 
 const View = SafeAreaView;
 
+const redirect_uri = "exp://localhost:19000/oauth/redirect";
+
 export default LoginScreen = ({ navigation }) => {
   return (
     <View>
       <Container>
         <LogoImage source={require("../../assets/logo.png")} />
         <Margin size={50 } />
-        <OAuthLoginButton navigation={navigation} />
+        <Link to="http://ec2-3-35-210-123.ap-northeast-2.compute.amazonaws.com/oauth2/authorization/kakao?redirect_uri={redirect_uri}">
+          <OAuthLoginButton />
+        </Link>
+        {/* <OAuthLoginButton navigation={navigation} /> */}
       </Container>
     </View>
   );
