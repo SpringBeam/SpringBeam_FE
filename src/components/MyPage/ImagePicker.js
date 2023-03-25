071b54e0824b2b1bee2a7aee10f874d7ecadd93b
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-03-25 17:45:41 +0900

    feat: 카메라 기능 구현 중
    feat: 카메라 기능 구현 중

--- src/components/MyPage/ImagePicker.js
+++ src/components/MyPage/ImagePicker.js
@@ -1,31 +1,30 @@
 import { PermissionsAndroid } from "react-native";
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 export const showPicker = async () => {
   const grantedcamera = await PermissionsAndroid.request(
     PermissionsAndroid.PERMISSIONS.CAMERA,
     {
-      title: "App Camera Permission",
-      message: "App needs access to your camera",
-      buttonNeutral: "Ask Me Later",
-      buttonNegative: "Cancel",
-      buttonPositive: "OK"
+      title: "앱 내 카메라 허용",
+      message: "해당 어플리케이션이 카메라 앱을 사용하고자 합니다!",
+      buttonNeutral: "다음에 다시 묻기",
+      buttonNegative: "취소",
+      buttonPositive: "허용"
     }
   );
 
   const grantedstorage = await PermissionsAndroid.request(
     PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
     {
-      title: "App Storage Permission",
-      message: "App needs access to your storage",
-      buttonNeutral: "Ask Me Later",
-      buttonNegative: "Cancel",
-      buttonPositive: "OK"
+      title: "앱 내 저장소 허용",
+      message: "해당 어플리케이션이 저장소 앱을 사용하고자 합니다!",
+      buttonNeutral: "다음에 다시 묻기",
+      buttonNegative: "취소",
+      buttonPositive: "허용"
     }
   );
 
   if (grantedcamera === PermissionsAndroid.RESULTS.GRANTED && grantedstorage === PermissionsAndroid.RESULTS.GRANTED) {
-    console.log("Camera & Storage Permission Given!");
+    console.log("Camera Permission Accessed.")
   }
   else {
     console.log("Camera Permission Denied.");
--- src/components/MyPage/ProfileImage.js
+++ src/components/MyPage/ProfileImage.js
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
+import { Alert } from "react-native";
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 import { showPicker } from "./ImagePicker";
 
 export default ProfileImage = () => {
 
+  const [ selectedImage, setSelectedImage ] = useState(null);
+
+  const handleLaunchCamera = () => {
+    launchCamera({ mediaType: "photo"}, (response) => {
+      if (response.didCancel) {
+        console.log("User cancelled Image Picker");
+      } else if (response.error) {
+        console.log("ImagePicker Error: ", response.error);
+      } else {
+        const source = {uri: response.uri};
+        setSelectedImage(source);
+      }
+    });
+  };
+
+  const handleLaunchImageLibrary = () => {
+    launchImageLibrary({ mediaType: "photo"}, (response) => {
+      if (response.didCancel) {
+        console.log("User cancelled Image Picker");
+      } else if (response.error) {
+        console.log("ImagePicker Error: ", response.error);
+      } else {
+        const source = {uri: response.uri};
+        setSelectedImage(source);
+      }
+    });
+  };
+
   const onPressHandler = () => {
-    showPicker();
+    Alert.alert(
+      "프로필 사진 업로드",
+      "프로필 사진을 올려주세요!",
+      [
+        {
+          text: "카메라로 업로드",
+          onPress: async() => {handleLaunchCamera()}
+        },
+        {
+          text: "앨범에서 업로드",
+          onPress: async() => {handleLaunchImageLibrary()}
+        },
+      ],
+      {cancelable: false}
+    )
   };
 
   return (
     <ImageContainer
-      onPress={() => onPressHandler()}
+      onPress={() => {onPressHandler()}}
     >
       
     </ImageContainer>
